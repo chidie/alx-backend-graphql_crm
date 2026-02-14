68c4ebb00bdbd1fa8b183bd6c12f81dcda20c5a5
Chidiebere Onuoha <chidieonuoha@gmail.com>
2026-02-14 23:08:26 +0100

    cleanup5

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,10 +1,6 @@
 import graphene
 
-class CRMQuery(graphene.ObjectType):
-    # later you will add customer, leads, etc.
-    pass
-
-class Query(graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
